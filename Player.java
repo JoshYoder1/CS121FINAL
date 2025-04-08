35c3e2d809184c3b225bd676492276b7f8996f55
JoshYoder1 <josh.yoder@bsu.edu>
2025-04-08 18:55:25 +0000

    create PLayer.java interface

---
+++ Player.java
@@ -0,0 +1,9 @@
+// Player.java
+interface Player{
+	protected ArrayList<String> hand = new ArrayList<String>();
+	protected int handAmount;
+	
+	public void hit();
+	public void stand();
+	public void bust();
+}//end interface
