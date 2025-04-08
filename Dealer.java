38a7b896c3f3162528783c044f3bde2c4806244b
JoshYoder1 <josh.yoder@bsu.edu>
2025-04-08 21:03:40 +0000

    update Dealer.java

--- Dealer.java
+++ Dealer.java
@@ -6,6 +6,7 @@ public class Dealer{
 	private int soft;
 
 	public static void main(String[] args){
+		System.out.println("Testing Dealer");
 		Dealer test = new Dealer();
 		System.out.println(test.hand);
 		test.hit();
