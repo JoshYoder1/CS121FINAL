83b9c144fcc9b1a583dae2f651a7d45b4db6defe
JoshYoder1 <josh.yoder@bsu.edu>
2025-04-16 13:50:39 +0000

    update

---
+++ Venue.java
@@ -0,0 +1,10 @@
+//Venue.java
+public class Venue{
+	private String City;
+	private String Name;
+	private String Phone;
+	private String URL;
+	private String Capacity;
+	private String Style;
+
+}//end class
