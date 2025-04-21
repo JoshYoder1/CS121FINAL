1926657a1807ed7b55bbc5c6687cbcbed192188c
JoshYoder1 <josh.yoder@bsu.edu>
2025-04-21 15:58:57 +0000

    update

---
+++ Program.java
@@ -0,0 +1,10 @@
+//Program.java
+public class Program{
+	
+	public static void main(String[] args){
+		
+	}//end main
+	
+	
+	
+}//end class
--- Venue.java
+++ Venue.java
@@ -5,6 +5,38 @@ public class Venue{
 	private String Phone;
 	private String URL;
 	private String Capacity;
-	private String Style;
+	private Arraylist<String> Style;
 
+	public Venue(){
+		City = null;
+		Name = null;
+		Phone = null;
+		URL = null;
+		Capacity = null;
+		Style = new Arraylist<String>();
+	}//end constuctor
+
+	public void setCity(String city){
+		City = city;
+	}//end setCity
+
+	public void setName(name){
+		Name = name;
+	}//end setName
+
+	public void setPhone(phone){
+		Phone = phone;
+	}//end setPhone
+
+	public void setURL(url){
+		URL = url;
+	}//end setURL
+
+	public void setCapacity(capacity){
+		Capacity = capacity;
+	}//end csetCapacity
+
+	public void setStyle(style){
+		Style = style;
+	}//end setStyle
 }//end class
